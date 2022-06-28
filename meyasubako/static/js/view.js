ec58400f07984eda264c744e96cdf1d561afcc36
Ao <sss.seitokai960@gl.pen-kanagawa.ed.jp>
2022-06-29 05:46:32 +0900

    smartphone

---
+++ meyasubako/static/js/view.js
@@ -0,0 +1,14 @@
+// const resizeWindow = () =>{
+//   let textElem = document.getElementsByClassName('opinion_title');
+//   for(i=0;i<textElem.length;i++){
+//     let elem = textElem[i];
+//     let textLength = elem.textContent.length;
+//     console.log(textLength);
+//     console.log(Math.floor(100/textLength));
+//     elem.style.fontSize= Math.floor(100/textLength)+'%';
+//   }
+// }
+
+// resizeWindow();
+
+// window.addEventListener('resize', resizeWindow);
