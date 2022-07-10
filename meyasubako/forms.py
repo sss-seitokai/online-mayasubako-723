5c7ba4f67cbaec288675c68d9ff6f1b68218916e
Ao <sss.seitokai960@gl.pen-kanagawa.ed.jp>
2022-07-10 09:10:17 +0900

    third

--- meyasubako/forms.py
+++ meyasubako/forms.py
@@ -8,7 +8,7 @@ class Form(forms.ModelForm):
     labels = {'title':'タイトル(〇〇の改善要求など)','content':'本文','who':'お名前(匿名希望の場合は「匿名希望」)','student_number':'生徒番号'}
     widgets = {
       'title':forms.TextInput(attrs={'placeholder':'例)〇〇の修理について'}),
-      'content':forms.Textarea(attrs={'placeholder':'本文をお書きください'}),
+      'content':forms.Textarea(attrs={'placeholder':'ここに本文をお書きください'}),
       'who':forms.TextInput(attrs={'placeholder':'◯年◯組中等太郎'}),
       'student_number':forms.NumberInput(attrs={'placeholder':'00000'}),
     }
