a3fc2d1d7f791da0764565859ed06e84ba3e79cb
Ao <sss.seitokai960@gl.pen-kanagawa.ed.jp>
2022-07-17 12:17:45 +0900

    if lose

--- online_meyasubako/settings.py
+++ online_meyasubako/settings.py
@@ -127,7 +127,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -151,7 +150,7 @@ if not DEBUG:
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     # Static files (CSS, JavaScript, Images)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
     STATIC_URL = '/static/'
 
     # Extra places for collectstatic to find static files.
