be851151b415e8434ecd3da6f20d3d81bc5e54f6
aoikeda <aaah3100@gmail.com>
2022-08-13 18:26:30 +0900

    localsucceeded

---
+++ online_meyasubako/settings_local.py
@@ -0,0 +1,11 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SECRET_KEY = 'django-insecure-%cqnj&mh!1&z!w-$aj9nfn5dnnw#=g7dr(&0&2jan66(&m%ycu'
+
+DATABASES = {
+ 'default': {
+ 'ENGINE': 'django.db.backends.sqlite3',
+ 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ }
+}
+DEBUG = True
