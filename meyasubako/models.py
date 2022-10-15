b08b71d894687154f265e0b984fe7ec3cacfacf9
aoikeda <aaah3100@gmail.com>
2022-10-16 08:55:16 +0900

    something

--- meyasubako/models.py
+++ meyasubako/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# user = User.objects.get(id=1)
 # Create your models here.
 class Opinion(models.Model):
   title = models.CharField(max_length=16) 
   content = models.CharField(max_length=10000)
   who = models.CharField(max_length=50)
-  student_email = models.CharField(max_length=50,default='example@email.com')
+  student_email = models.CharField(max_length=255,default='example@gmail.com')
   date_added = models.DateTimeField(auto_now_add=True)
   status = models.IntegerField(default=1)
   def __str__(self):
